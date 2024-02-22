516ff9ab3adb95738ffdef4d046b6a2030b13e80
Arvindraj <harijana.bsc23@rvu.edu.in>
2024-02-22 23:02:45 +0530

    test

---
+++ set02/problem02.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+float input_side();
+int check_scalene(float a, float b, float c);
+void output(int isscalene);
+
+int main()
+
+{
+    float s1,s2,s3,t;
+    s1 = input_side();
+    s2 = input_side();
+    s3 = input_side();
+    t = check_scalene(s1,s2,s3);
+    output(t);
+}
+
+float input_side()
+{
+    float a;
+    printf("Enter the first side of the triangle: \n");
+    scanf("%f",&a);
+    return a;
+}
+
+int check_scalene(float a, float b, float c)
+{
+    if(a!=b && a!=c)
+    {
+        return 2;
+    }
+    else
+    {
+        return 3;
+    }
+}
+
+void output(int isscalene)
+{
+    if(isscalene == 2)
+    {
+        printf("The triangle is scalene");
+    }
+    else
+    {
+        printf("The triangle is not scalene");
+    }
+}
